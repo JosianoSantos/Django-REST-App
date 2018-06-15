66764646cf83ef8ac65453c28a01be4068c2cd74
Josiano Santos <josiano@fp2.com.br>
2018-06-15 15:21:54 -0300

    no message

--- src/User/urls.py
+++ src/User/urls.py
@@ -6,9 +6,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rest_example.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^user/', views.UserList.as_view()),
